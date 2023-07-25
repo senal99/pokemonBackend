4c74386ad3481ad6781fa0b5dff21163d174e8e7
senal99 <senal.weerasekara@insighture.com>
2023-07-25 17:46:49 +0530

    docker added

--- src/schema/pokemon.ts
+++ src/schema/pokemon.ts
@@ -1,8 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import { HydratedDocument } from 'mongoose'
 
-// export type PokemonDocument = HydratedDocument<Pokemon>;
-
 @Schema({
     timestamps:true
 })
